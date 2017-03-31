367785a46ab5f97ddc74d2323700ae1404b997e0
Thomas Viau <thomas.viau@eisti.fr>
2017-04-01 01:02:16 +0200

    feature/material: added tabs + material icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,26 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
+import { InstallationComponent } from './installation/installation.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InstallationComponent,
+    ConfigurationComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
